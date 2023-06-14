4d096c1e1ad991e4ef94dcd70d5161aff0bec1d8
helencristina19 <helensouzza8@gmail.com>
2023-06-14 11:50:52 -0300

    Novas Alterações

---
+++ cypress/e2e/navbar.cy.js
@@ -0,0 +1,21 @@
+describe('Testes da navbar', () => {
+
+    beforeEach (() => {
+        cy.visit('/index.html')
+    });
+
+    it('Deve ter a classe .navbar', () => {
+
+        cy.get('nav').should('have.class', 'navbar')
+
+    });
+
+    it('Deve ter um atributo href válido para cada página', () => {
+
+        cy.get('[data-test=home]').should('have.attr', 'href', './index.html');
+        cy.get('[data-test=sobre-nos]').should('have.attr', 'href', './sobre-nos.html');
+        cy.get('[data-test=catalogo]').should('have.attr', 'href', './catalogo.html');
+        cy.get('[data-test=contato]').should('have.attr', 'href', './contato.html');
+
+});
+});
